9f43422f568f16f1660c77b1e07410afdb7d035a
developerMohib <92154638+developerMohib@users.noreply.github.com>
2023-05-27 12:32:02 +0600

    Odd & Even Num Using For Loop And continue Keyword

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// Print the odd number from 1 to 100
+
+for(let i = 1; i <= 100; i++ ){
+  if(i % 2 == 0)
+  continue;
+  document.write(" "+ i)
+}
+document.write( ' <br> End the odd Number. <br> ')
+
+// Print the even number from 1 to  1000
+
+for (let e = 0; e <= 100; e++ ) {
+  if (e % 2 !=0 ) {
+    continue;
+  }
+  document.write(" " + e)
+}
